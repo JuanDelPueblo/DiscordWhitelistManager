d79ebdf45e73d6e6cb37208716b4b9ddb69116ee
JuanDelPueblo <mcpuebloserver@gmail.com>
2020-06-25 11:17:31 -0400

    Make sure player is unwhitelisted even when offline, bump version

--- src/main/java/net/mcpueblo/discordwhitelistmanager/utils/RemoveWhitelistUtil.java
+++ src/main/java/net/mcpueblo/discordwhitelistmanager/utils/RemoveWhitelistUtil.java
@@ -1,18 +1,25 @@
 package net.mcpueblo.discordwhitelistmanager.utils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
 public class RemoveWhitelistUtil {
     public static String removeWhitelist(UUID mcUUID) {
-        Player player = Bukkit.getPlayer(mcUUID);
-        String playerName = player.getName();
         if (!(Bukkit.getPlayer(mcUUID) == null)) {
+            Player player = Bukkit.getPlayer(mcUUID);
+            String playerName = player.getName();
             player.setWhitelisted(false);
             return playerName;
+        } else if (!(Bukkit.getOfflinePlayer(mcUUID) == null)) {
+            OfflinePlayer player = Bukkit.getOfflinePlayer(mcUUID);
+            String playerName = player.getName();
+            player.setWhitelisted(false);
+            return playerName;
+        } else {
+            return null;
         }
-        return null;
     }
 }
